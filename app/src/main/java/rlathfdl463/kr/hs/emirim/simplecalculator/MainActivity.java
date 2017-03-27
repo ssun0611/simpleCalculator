07cfab1a8acc74a139c56e2a0ce78cde9b11f40f
ssun0611 <rlathfdl463@naver.com>
2017-03-27 20:00:15 +0900

    #1 XML Layout

---
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package rlathfdl463.kr.hs.emirim.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
